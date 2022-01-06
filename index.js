87298adcc239389b3390069504493ea1b41daac3
benito <benideveloperr@gmail.com>
2022-01-05 22:02:20 -0300

    port-heroku

--- index.js
+++ index.js
@@ -85,7 +85,7 @@ app.use((req, res) =>
   })
 );
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log("hello wordddddd from NODE JSS OMGG!!!!");
 });
